9d291f4c0e6d2daf9d43f2d11f9ed278ad78c7dc
DAVID <gabriel3052000@yahoo.com>
2018-06-22 16:25:54 -0700

    Working on SessionForm date and time

--- src/com/sessionnotes/view/createsessionjframe/CreateSessionJFrame.java
+++ src/com/sessionnotes/view/createsessionjframe/CreateSessionJFrame.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Properties;
 
@@ -21,6 +22,11 @@ import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.UtilDateModel;
 
 import com.toedter.calendar.JDateChooser;
+import com.github.lgooddatepicker.components.DateTimePicker;
+import com.github.lgooddatepicker.components.DatePicker;
+import com.github.lgooddatepicker.components.DatePickerSettings;
+import javax.swing.ImageIcon;
+import com.github.lgooddatepicker.components.TimePicker;
 
 
 public class CreateSessionJFrame extends JFrame {
@@ -54,7 +60,6 @@ public class CreateSessionJFrame extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public CreateSessionJFrame() {
 		setTitle("Session Notes - Create New Session");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -102,76 +107,44 @@ public class CreateSessionJFrame extends JFrame {
 		JLbl_ClientPhone.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		JPanel_Labels.add(JLbl_ClientPhone);
 		
-		JPanel panel = new JPanel();
-		sl_contentPane.putConstraint(SpringLayout.NORTH, panel, 46, SpringLayout.SOUTH, JPanel_Labels);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, panel, 84, SpringLayout.SOUTH, JPanel_Labels);
-		panel.setBackground(Color.WHITE);
-		sl_contentPane.putConstraint(SpringLayout.WEST, panel, 0, SpringLayout.WEST, JLbl_NewSessionForm);
-		sl_contentPane.putConstraint(SpringLayout.EAST, panel, 13, SpringLayout.EAST, JPanel_Labels);
-		contentPane.add(panel);
-		SpringLayout sl_panel = new SpringLayout();
-		panel.setLayout(sl_panel);
+		JPanel JPanel_SessionDate = new JPanel();
+		sl_contentPane.putConstraint(SpringLayout.WEST, JPanel_SessionDate, 0, SpringLayout.WEST, JLbl_NewSessionForm);
+		JPanel_SessionDate.setBackground(Color.WHITE);
+		contentPane.add(JPanel_SessionDate);
 		
-		JDateChooser dateChooser = new JDateChooser();
-		sl_panel.putConstraint(SpringLayout.NORTH, dateChooser, 10, SpringLayout.NORTH, panel);
-		sl_panel.putConstraint(SpringLayout.WEST, dateChooser, 88, SpringLayout.WEST, panel);
-		sl_panel.putConstraint(SpringLayout.EAST, dateChooser, 0, SpringLayout.EAST, panel);
-		dateChooser.setDate(new Date());
-		panel.add(dateChooser);
+		DatePicker DatePicker_SessoinDate = new DatePicker(getDatePickerSettings());
+		DatePicker_SessoinDate.getComponentToggleCalendarButton().setText("");
+		DatePicker_SessoinDate.getComponentToggleCalendarButton().setIcon(new ImageIcon(CreateSessionJFrame.class.getResource("/images/datepickerbutton1.png")));
+		DatePicker_SessoinDate.setDateToToday();
+		JPanel_SessionDate.add(DatePicker_SessoinDate);
 		
-		JLabel lblSessionDate = new JLabel("Session Date");
-		sl_panel.putConstraint(SpringLayout.NORTH, lblSessionDate, 10, SpringLayout.NORTH, panel);
-		sl_panel.putConstraint(SpringLayout.WEST, lblSessionDate, 0, SpringLayout.WEST, panel);
-		panel.add(lblSessionDate);
+		TimePicker TimePicker_SessionTime = new TimePicker();
+		JPanel_SessionDate.add(TimePicker_SessionTime);
 		
-		JPanel panel_1 = new JPanel();
-		sl_contentPane.putConstraint(SpringLayout.NORTH, panel_1, 10, SpringLayout.SOUTH, panel);
-		sl_contentPane.putConstraint(SpringLayout.WEST, panel_1, 0, SpringLayout.WEST, JLbl_NewSessionForm);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, panel_1, -197, SpringLayout.SOUTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, panel_1, -420, SpringLayout.EAST, contentPane);
-		panel_1.setBackground(Color.WHITE);
-		contentPane.add(panel_1);
-		SpringLayout sl_panel_1 = new SpringLayout();
-		panel_1.setLayout(sl_panel_1);
+		JLabel JLbl_DateTime = new JLabel("Session Date and Time ");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, JPanel_SessionDate, 6, SpringLayout.SOUTH, JLbl_DateTime);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, JLbl_DateTime, 20, SpringLayout.SOUTH, JPanel_Labels);
+		sl_contentPane.putConstraint(SpringLayout.WEST, JLbl_DateTime, 0, SpringLayout.WEST, JLbl_NewSessionForm);
+		contentPane.add(JLbl_DateTime);
+		JLbl_DateTime.setFont(new Font("Tahoma", Font.BOLD, 12));
 		
-		JLabel lblSessionTime = new JLabel("Session Time");
-		sl_panel_1.putConstraint(SpringLayout.NORTH, lblSessionTime, 10, SpringLayout.NORTH, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.WEST, lblSessionTime, 0, SpringLayout.WEST, panel_1);
-		panel_1.add(lblSessionTime);
-		
-		JComboBox comboBox = new JComboBox();
-		sl_panel_1.putConstraint(SpringLayout.NORTH, comboBox, -3, SpringLayout.NORTH, lblSessionTime);
-		sl_panel_1.putConstraint(SpringLayout.WEST, comboBox, 27, SpringLayout.EAST, lblSessionTime);
-		sl_panel_1.putConstraint(SpringLayout.EAST, comboBox, 0, SpringLayout.EAST, panel_1);
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"12:00 AM", "12:30 AM", "1:00 AM", "1:30 AM", "2:00 AM", "2:30 AM", "3:00 AM", "3:30 AM", "4:00 AM", "4:30 AM", "5:00 AM", "5:30 AM", "6:00 AM", "6:30 AM", "7:00 AM", "7:30 AM", "8:00 AM", "8:30 AM", "9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM", "12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM", "9:30 PM", "10:00 PM", "10:30 PM", "11:00 PM", "11:30 PM"}));
-		comboBox.setSelectedIndex(24);
-		panel_1.add(comboBox);
 		
 
 	}
 	
 	
-	
+
 	/**
-	 * Makes a fully formatted JDatePicker to be used on a 
-	 * JFrame.
-	 * @return JDatePickerImpl needs to be added to a JFrame or panel.
+	 * 
+	 * @return
 	 */
-	private JDatePickerImpl getDatePickerComponent(){		
-		Properties p = new Properties();
-		p.put("text.today", "Today");
-		p.put("text.month", "Month");
-		p.put("text.year", "Year");
-		UtilDateModel model = new UtilDateModel();
-		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
-		JDatePickerImpl datePickerImpl = new JDatePickerImpl(datePanel, new DateComponentFormatter());
-		datePickerImpl.getJFormattedTextField().setText(getCurrentDate());
-		datePickerImpl.getJFormattedTextField().setToolTipText("Click button on right to open calendar.");
-	
-				
-		return datePickerImpl;
+	private DatePickerSettings getDatePickerSettings() {
+		DatePickerSettings dateSettings = new DatePickerSettings();
+		dateSettings.setAllowKeyboardEditing(false);
+
+		return dateSettings;
 	}
-	
+
 	
 	/**
 	 * Returns current date in the same format
@@ -180,9 +153,7 @@ public class CreateSessionJFrame extends JFrame {
 	 */
 	private String getCurrentDate() {
 		Date now = new Date();
-		SimpleDateFormat dateFormat = new SimpleDateFormat("MMM d, yyyy");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM d, yyyy");
 		return dateFormat.format(now);
 	}
-	
-	
 }
